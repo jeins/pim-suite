98311fb49ba19a08b86bc1b1fb300b39f838f3e7
jeins <mail@mjuanakbar.info>
2016-10-26 10:31:42 +0200

    added  entitiy framework

---
+++ PimSuite/Controllers/DashboardController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PimSuite.Models;
+
+namespace PimSuite.Controllers
+{
+    public class DashboardController : Controller
+    {
+        private PimSuiteDatabaseEntities _db;
+        public DashboardController()
+        {
+            _db = new PimSuiteDatabaseEntities();
+        }
+        // GET: Dashboard
+        public ActionResult Index()
+        {
+            var allUsers = _db.Users.ToList();
+            var b = "";
+            return View();
+        }
+    }
+}
