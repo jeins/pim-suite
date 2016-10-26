92e91721a9f2f8385cd76e04d1500773b3af2c6d
Muhammad Juan Akbar <mail@mjuan.me>
2016-10-26 16:32:06 +0200

    added other sample, UserController (Login & Register)

---
+++ PimSuite/Controllers/UserController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PimSuite.Models;
+
+namespace PimSuite.Controllers
+{
+    public class UserController : Controller
+    {
+
+        //GET User/Login
+        public ActionResult Login(UserModel model)
+        {
+            if (model.Email != null && model.Password != null)
+            {
+                return RedirectToRoute(new {controller = "Dashboard", action = "Index"});
+            }
+
+            return View();
+        }
+
+        //GET User/Register
+        public ActionResult Register(UserModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            if (model.Email != null && model.FirstName != null && model.LastName != null && model.Password != null)
+            {
+                return RedirectToRoute(new { controller = "Dashboard", action = "Index" });
+            }
+            return View();
+        }
+    }
+}
