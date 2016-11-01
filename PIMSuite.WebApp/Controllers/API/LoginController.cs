9a19a8313c3ea739ed291fd4b616f1adf1b7a308
Nicolas Mehlei <nicolas.mehlei@gmail.com>
2016-11-01 10:18:13 +0100

    added new solution

---
+++ PIMSuite.WebApp/Controllers/API/LoginController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace PIMSuite.WebApp.Controllers.API
+{
+    public class LoginController : ApiController
+    {
+        public sealed class LoginModel
+        {
+            public string EmailAddress { get; set; }
+            public string Password { get; set; }
+        }
+
+        [HttpPost]
+        public HttpResponseMessage Login(LoginModel model)
+        {
+            // validate
+
+            // set cookie
+
+            return Request.CreateResponse(HttpStatusCode.Accepted, true);
+        }
+
+        public sealed class RegisterModel
+        {
+            //TODO
+        }
+
+        [HttpPost]
+        public HttpResponseMessage Register(RegisterModel model)
+        {
+            //TODO
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PIMSuite.WebApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PIMSuite.WebApp.MvcApplication" Language="C#" %>
