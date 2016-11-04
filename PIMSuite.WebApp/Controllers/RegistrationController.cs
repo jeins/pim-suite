be94f9e4b84cbd63781fbe446c14dc32e3cde1fc
AnnaMyak <myakinen@gmail.com>
2016-11-04 03:11:04 +0100

    Registration

---
+++ PIMSuite.WebApp/Controllers/RegistrationController.cs
@@ -0,0 +1,42 @@
+﻿using PIMSuite.WebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PIMSuite.WebApp.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        // GET: Registration
+        public ActionResult Index()
+        {
+            using (PIMDBEntities db = new PIMDBEntities())
+            {
+                return View(db.User.ToList());
+            }
+        }
+
+        public ActionResult Registration()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Registration(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                using (PIMDBEntities db= new PIMDBEntities())
+                {
+                    db.User.Add(user);
+                    db.SaveChanges();
+                }
+                ModelState.Clear();
+                ViewBag.Message = user.Vorname+" "+user.Nachname+ " "+ "wurde erfolgreich registriert!";
+            }
+            return View();
+        }
+    }
+}
