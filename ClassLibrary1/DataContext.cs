26df5dc5d4d270bdfe21e026b7dd5f27b1a7e762
AnnaMyak <myakinen@gmail.com>
2016-11-04 05:26:21 +0100

    Registration 2.0

---
+++ ClassLibrary1/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataPersistence
+{
+    public class DataContext : DbContext
+    {
+        public DataContext() : base("PIMDB")
+        { }
+        public DbSet<User> Users { get; set;}
+    }
+}
---
+++ PIMFinal/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PIMFinal.MvcApplication" Language="C#" %>
