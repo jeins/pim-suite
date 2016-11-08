575a0ab0ea71a9783ab4c2f242e4190af1a950a9
Muhammad Juan Akbar <mail@mjuan.me>
2016-11-08 18:50:41 +0100

    14-ang. Nutzer: gesamtes Nutzerverzeichnis #12 - added ProfileController - design /Profile/Index - added sort by lastname (asc / desc)

---
+++ PIMSuite.WebApp/Controllers/ProfileController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PIMSuite.Persistence;
+using PIMSuite.Persistence.Entities;
+
+namespace PIMSuite.WebApp.Controllers
+{
+    public class ProfileController : Controller
+    {
+        private DataContext _dataContext;
+
+        public ProfileController()
+        {
+            _dataContext = new DataContext();;
+        }
+
+        // GET: Profile
+        public ActionResult Index(string sort)
+        {
+            //TODO:: entities should be in english
+            var users = from u in _dataContext.Users
+                        select u;
+
+            switch (sort)
+            {
+                case "desc":
+                    users = users.OrderByDescending(u => u.Nachname);
+                    ViewBag.AvailableSortType = "asc";
+                    break;
+                default:
+                    users = users.OrderBy(u => u.Nachname);
+                    ViewBag.AvailableSortType = "desc";
+                    break;
+            }
+            
+            return View(users.ToList());
+        }
+    }
+}
