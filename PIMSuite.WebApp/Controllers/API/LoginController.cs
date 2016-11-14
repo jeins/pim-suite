b28b708819583270371a8b7b00b4ded1f8d0a8ce
Dustin F. <bosche@hotmail.de>
2016-11-14 17:19:31 +0100

    Erster Annäherungsversuch^^

--- PIMSuite.WebApp/Controllers/API/LoginController.cs
+++ PIMSuite.WebApp/Controllers/API/LoginController.cs
@@ -1,14 +1,18 @@
-﻿using System;
+﻿using PIMSuite.Persistence;
+using PIMSuite.Persistence.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 
+
 namespace PIMSuite.WebApp.Controllers.API
 {
     public class LoginController : ApiController
     {
+
         public sealed class LoginModel
         {
             public string EmailAddress { get; set; }
@@ -19,10 +23,27 @@ namespace PIMSuite.WebApp.Controllers.API
         public HttpResponseMessage Login(LoginModel model)
         {
             // validate
+            User user;
+            if (ModelState.IsValid)
+            {
+                using (DataContext context = new DataContext())
+                {
+                    user = context.Users.Single(p => p.Email == model.EmailAddress);
+                }
+                if (user != null && string.Equals(user.Password, model.Password))
+                {
+                    ModelState.Clear();
+                    return Request.CreateResponse(HttpStatusCode.Accepted, true);
+                    //TODO: 
+                }
+                else
+                {
+                    return Request.CreateErrorResponse(HttpStatusCode.Unauthorized, "False password!");
+                }     
 
-            // set cookie
-
-            return Request.CreateResponse(HttpStatusCode.Accepted, true);
+                
+            }
+            return Request.CreateErrorResponse(HttpStatusCode.Unauthorized, "Missing or wrong user credentials!");
         }
 
         public sealed class RegisterModel
