2e3801431037488a5fc9f208f3bf67a6585d640c
Nicolas Mehlei <nicolas.mehlei@gmail.com>
2016-11-21 12:44:20 +0100

    added utility project with hash helper class

---
+++ PIMSuite.Utilities/Auth/IHashHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace PIMSuite.Utilities.Auth
+{
+    public interface IHashHelper
+    {
+        String Hash(String text);
+        bool Verify(String text, String saltedHash);
+    }
+
+    public class HashHelper : IHashHelper
+    {
+        private const int WorkFactor = 5;
+
+        public String Hash(String text)
+        {
+            var saltedHash = BCrypt.Net.BCrypt.HashPassword(text, WorkFactor);
+            return saltedHash;
+        }
+
+        public bool Verify(String text, String saltedHash)
+        {
+            return BCrypt.Net.BCrypt.Verify(text, saltedHash);
+        }
+    }
+}
