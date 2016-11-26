99b42a0a852b5a601ade14645e0648916b4ff7ca
Muhammad Juan Akbar <mail@mjuan.me>
2016-11-26 10:34:29 +0100

    #39 - added Message Entity - added Connection Entity

---
+++ PIMSuite.Persistence/Entities/Connection.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PIMSuite.Persistence.Entities
+{
+    public class Connection
+    {
+        public Connection()
+        {       
+            Id = Guid.NewGuid();
+        }
+
+        [Key]
+        public Guid Id { get; set; }
+
+        public string ConnectionId { get; set; }
+
+        public Guid UserId { get; set; }
+
+        public virtual User User { get; set; }
+    }
+}
---
+++ PIMSuite.Persistence/Entities/Message.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PIMSuite.Persistence.Entities
+{
+    public class Message
+    {
+        public Message()
+        {
+            MessageId = Guid.NewGuid();
+            CreatedAt = DateTime.Now;
+        }
+
+        [Key]
+        public Guid MessageId { get; set; }
+        
+        [ForeignKey("User")]
+        public Guid SenderUserId { get; set; }
+
+        [ForeignKey("User")]
+        public Guid ReceiverUserId { get; set; }
+
+        public string MessageBody { get; set; }
+
+        public bool IsRead { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+    }
+}
