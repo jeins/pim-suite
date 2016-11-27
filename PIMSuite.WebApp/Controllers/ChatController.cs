25ef78783c75a21b64ce31e72a0713e59476fd87
Muhammad Juan Akbar <mail@mjuan.me>
2016-11-27 13:04:46 +0100

    #41 - added ChatButton on Profile - redirect from profile to chat room

--- PIMSuite.WebApp/Controllers/ChatController.cs
+++ PIMSuite.WebApp/Controllers/ChatController.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using PIMSuite.Persistence;
 using PIMSuite.Persistence.Entities;
+using PIMSuite.Persistence.Repositories;
 
 namespace PIMSuite.WebApp.Controllers
 {
@@ -12,19 +13,29 @@ namespace PIMSuite.WebApp.Controllers
     {
 
         private DataContext _dataContext;
+        private IMessageRepository _messageRepository;
 
         public ChatController()
         {
             _dataContext = new DataContext();
+            _messageRepository = new MessageRepository(_dataContext);
         }
 
         // GET: Chat
-        public ActionResult Index()
+        public ActionResult Index(string toUserId)
         {
             User currentUser = ViewBag.User; 
             var users = _dataContext.Users.Where(u => u.UserId != currentUser.UserId);
 
-            //ViewBag.UserList = users.ToList();
+            ViewBag.ChatToUserId = null;
+            ViewBag.ChatHistories = new string[] {};
+
+            if (toUserId != null)
+            {
+                ViewBag.ChatToUserId = toUserId;
+                ViewBag.ChatHistories = _messageRepository.GetMessageHistories(currentUser.UserId, new Guid(toUserId));
+
+            }
 
             return View(users);
         }
