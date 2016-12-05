36b785a002d0cbc28f1f165006e8e52c9a76d2f2
Dustin F. <bosche@hotmail.de>
2016-12-05 20:51:58 +0100

    Bug in ResetPW behoben

--- PIMSuite.WebApp/Controllers/ResetPWController.cs
+++ PIMSuite.WebApp/Controllers/ResetPWController.cs
@@ -46,14 +46,16 @@ namespace PIMSuite.WebApp.Controllers
                 user = context.Users.SingleOrDefault(p => p.Email == userEmail.Email);
                 if (user == null)
                 {
-                    return new HttpStatusCodeResult(HttpStatusCode.Forbidden, "Wrong credentials!   Test");
+                    //return new HttpStatusCodeResult(HttpStatusCode.Forbidden, "Wrong credentials!   Test");
+                    ViewBag.Message = "No account was found with this email";
                 }
                 else 
                 {
                     var hh = new HashHelper();                    
                     var tempPassword = TokenGenerator.GenerateValidationToken(passwordTokenSize);
                     user.Password = hh.Hash(tempPassword);
-                    userRepository.UpdateUser(user);                                
+                    userRepository.UpdateUser(user);
+                    userRepository.Save();
                     ViewBag.Message = "A new temporary password has been sent to your mail - address"; 
                     EmailHelper.SendMail("smtp.gmail.com", "PIMSuiteASP@gmail.com", "noreplyASP", user.Email, "Your password reset request for the PIM Suite", "Your new temporary password: " + tempPassword);
                     Response.Redirect("/?infoMessage=A new temporary password has been sent to your mail-address, please check your mails and use the new password!");
