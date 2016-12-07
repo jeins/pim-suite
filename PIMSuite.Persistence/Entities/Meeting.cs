5d996a999b356f4bbc648846d9f6ee6f95c08bb0
AnnaMyak <myakinen@gmail.com>
2016-12-07 14:10:21 +0100

    Meeting-Entity and Participation-Entity

---
+++ PIMSuite.Persistence/Entities/Meeting.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PIMSuite.Persistence.Entities
+{
+    public class Meeting
+    {
+        public Meeting()
+        {
+            MeetingId = Guid.NewGuid();
+            Creation = DateTime.Now;
+        }
+
+        [Key]
+        public Guid MeetingId { get; set;}
+        public String Privacy { get; set; }
+        public String Titel { get; set; }
+        public DateTime Date { get; set; }
+        public DateTime Time { get; set; }
+        public String Location { get; set; }
+        public DateTime Duration { get; set; }
+        public DateTime Creation { get; set; }
+    }
+}
---
+++ PIMSuite.Persistence/Entities/participateMeeting.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PIMSuite.Persistence.Entities
+{
+    public class participateMeeting
+    {
+        public participateMeeting()
+        {
+            ParticipationId = Guid.NewGuid();
+            Creation = DateTime.Now;
+        }
+        [Key]
+        public Guid ParticipationId { get; set; }
+        public Guid MeetingId { get; set; }
+        public Guid UserId { get; set; }
+        public DateTime Creation { get; set; }
+    }
+}
