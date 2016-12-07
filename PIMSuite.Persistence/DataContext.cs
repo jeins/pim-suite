d8ac6f6690da1a1938ed3c5413a8d0419596eaa8
AnnaMyak <myakinen@gmail.com>
2016-12-07 16:11:24 +0100

    update Datacontext

--- PIMSuite.Persistence/DataContext.cs
+++ PIMSuite.Persistence/DataContext.cs
@@ -18,24 +18,26 @@ namespace PIMSuite.Persistence
         public DbSet<Location> Locations { get; set; }
         public DbSet<Message> Messages { get; set; }
         public DbSet<Connection> Connections { get; set; }
+        public DbSet<Meeting> Meetings { get; set; }
+        public DbSet<participateMeeting> Participations { get; set; }
 
-//        protected override void OnModelCreating(DbModelBuilder modelBuilder)
-//        {
-//            //Database.SetInitializer(new CreateDatabaseIfNotExists<DbContext>());
-//            Database.SetInitializer(new DropCreateDatabaseIfModelChanges<DbContext>());
-//            modelBuilder.Entity<Message>()
-//                       .HasRequired(m => m.Sender)
-//                       .WithMany(u => u.SentMessages)
-//                       .HasForeignKey(m => m.SenderId)
-//                       .WillCascadeOnDelete(false);
-//
-//            modelBuilder.Entity<Message>()
-//                       .HasRequired(m => m.Receiver)
-//                       .WithMany(u => u.ReceivedMessages)
-//                       .HasForeignKey(m => m.ReceiverId)
-//                       .WillCascadeOnDelete(false);
-//                       
-//            base.OnModelCreating(modelBuilder);
-//        }
+        //        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        //        {
+        //            //Database.SetInitializer(new CreateDatabaseIfNotExists<DbContext>());
+        //            Database.SetInitializer(new DropCreateDatabaseIfModelChanges<DbContext>());
+        //            modelBuilder.Entity<Message>()
+        //                       .HasRequired(m => m.Sender)
+        //                       .WithMany(u => u.SentMessages)
+        //                       .HasForeignKey(m => m.SenderId)
+        //                       .WillCascadeOnDelete(false);
+        //
+        //            modelBuilder.Entity<Message>()
+        //                       .HasRequired(m => m.Receiver)
+        //                       .WithMany(u => u.ReceivedMessages)
+        //                       .HasForeignKey(m => m.ReceiverId)
+        //                       .WillCascadeOnDelete(false);
+        //                       
+        //            base.OnModelCreating(modelBuilder);
+        //        }
     }
 }
