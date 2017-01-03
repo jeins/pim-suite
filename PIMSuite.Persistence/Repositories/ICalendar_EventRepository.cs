b880a18f0d63073aa405879e0e4db9ed960aa04f
Daniel <daniel.schulz@live.de>
2017-01-03 01:01:29 +0100

    Löschen von Calendar_Events implementiert

--- PIMSuite.Persistence/Repositories/ICalendar_EventRepository.cs
+++ PIMSuite.Persistence/Repositories/ICalendar_EventRepository.cs
@@ -7,7 +7,8 @@ namespace PIMSuite.Persistence.Repositories
     public interface ICalendar_EventRepository
     {
         void InsertCalendar_Event(Calendar_Event ev);
-        void DeleteCalendar_Event(Guid eventId);
+        void DeleteCalendar_Event(int eventId);
+        Calendar_Event GetEvent(int eventId);
         IEnumerable<Calendar_Event> GetAllCalendar_EventByUserIdAndCalendarId(Guid userId, int calendarId);
         void Save(); 
     }
