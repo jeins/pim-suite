7c4fd73c98012230ce050921b75478dde0d68774
Nicolas Mehlei <nicolas.mehlei@gmail.com>
2017-01-03 14:02:50 +0100

    moved domain script to own file

---
+++ PIMSuite.WebApp/Scripts/Administration.js
@@ -0,0 +1,21 @@
+﻿var domainViewModel = {
+    newDomain: ko.observable(),
+    addDomain: function () {
+        $.ajax(
+        {
+            url: '/API/Domain/Add',
+            type: 'POST',
+            data: {
+                NewDomain: this.newDomain(),
+            },
+            dataType: 'json',
+            statusCode: {
+                200: function () {
+                    window.location.href = '/Dashboard/';
+                }
+            }
+        });
+    }
+}
+
+ko.applyBindings(domainViewModel, document.getElementById("domainBlock")[0]);
--- PIMSuite.WebApp/Scripts/DashBoard.js
+++ PIMSuite.WebApp/Scripts/DashBoard.js
@@ -14,25 +14,4 @@
     }
 };
 
-var domainViewModel = {
-    newDomain: ko.observable(),
-    addDomain: function () {
-        $.ajax(
-        {
-            url: '/API/Domain/Add',
-            type: 'POST',
-            data: {
-                NewDomain: this.newDomain(),
-            },
-            dataType: 'json',
-            statusCode: {
-                200: function () {
-                    window.location.href = '/Dashboard/';
-                }
-            }
-        });
-    }
-}
-
 ko.applyBindings(dashboardViewModel, document.getElementsByClassName('navbar')[0]);
-ko.applyBindings(domainViewModel, document.getElementById("domainBlock"));
