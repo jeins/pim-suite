99081ba436943b681325d7c9fee8fa1c222a8c56
Daniel <daniel.schulz@live.de>
2017-01-10 09:32:02 +0100

    Bugfixes

--- PIMSuite.WebApp/Scripts/ShowCalendar.js
+++ PIMSuite.WebApp/Scripts/ShowCalendar.js
@@ -100,83 +100,7 @@ var calendar = $('#calendar')
             $('#add_is_private').prop('checked', false);
             
 
-            $(".add_submit").on('click', function () {
-                var d1 = $('#add_start').val().split('-');
-                var d2 = $('#add_end').val().split('-');
-               
-                var start_month = parseInt(d1[1],10);
-                var end_month = parseInt(d2[1],10);
-                var dt12 = d1[2].split(" ");
-                var dt22 = d2[2].split(" ");
-                var start_year = parseInt(dt12[0],10);
-                var end_year = parseInt(dt12[0], 10);
-                var check = false;
-                if (start_month < end_month && start_year <= end_year)
-                {
-                     check = true;
-                }
-                if (start_month > end_month && start_year <= end_year)
-                {
-                    check = true;
-                }
-                
-                
-                if ($('#add_title').val().length != 0 && $('#add_start').val().length != 0 && $('#add_end').val().length != 0 && ($('#add_start').val() < $('#add_end').val() || check))
-                {
-                    var newEvent = {
-                        
-                        title: $('#add_title').val(),
-                        start: moment($('#add_start').val(), 'DD-MM-YYYY HH:mm').format("LLLL"),
-                        end: moment($('#add_end').val(), 'DD-MM-YYYY HH:mm').format("LLLL"),
-                        location: $('#add_location').val(),
-                        description: $('#add_description').val(),
-                        allDay: false,
-                        calendarId: calendarId,
-                        isPrivate: $('#add_is_private').prop('checked'),
-                        isInvite: false,
-                        isInviteProcessed: false,
-                        invites: []
-                    };
-
-                    calendar.fullCalendar('renderEvent', newEvent, true);
-                    calendar.fullCalendar('unselect');
-
-                    $.ajax({
-                        type: 'POST',
-                        url: '/API/CalendarEvent/CreateNewEvent',
-                        data: newEvent
-                    });
-
-                    $('.add_close').click();
-
-                    return false;
-                }
-
-                else {
-                    
-                    if ($('#add_start').val() > $('#add_end').val()&& check==false)
-                    {
-                        
-                        alert("Der Zeitraum für das Ereignis wurde nicht korrekt eingegeben! Versuchen Sie die Eingabe noch mal.");
-                    }
-                    if ($('#add_start').val() == $('#add_end').val()) {
-                        alert("Sie haben identische Werte für Start und Ende eigegeben. Versuchen Sie die Eingabe noch mal.");
-                    }
-                    if ($('#add_start').val().length == 0) {
-                        alert("Sie haben nichts für Start eingegeben");
-                    }
-                    if ($('#add_end').val().length == 0) {
-                        alert("Sie haben nichts für Ende eingegeben");
-                    }
-                    if ($('#add_title').val().length == 0) {
-                        alert("Sie haben nichts für Title eingegeben");
-                    }
-                    
-                    return true;
-                  
-                    
-                }
-            });
+            
 
         },
         eventClick: function(calEvent) {
@@ -237,6 +161,80 @@ $(function () {
                 self.val("abonnieren");
                 $.notify("Sie abonnieren den Kalender nicht mehr", "warn");
             }
+        }
+    });
+
+    $(".add_submit").on('click', function () {
+        var d1 = $('#add_start').val().split('-');
+        var d2 = $('#add_end').val().split('-');
+
+        var start_month = parseInt(d1[1], 10);
+        var end_month = parseInt(d2[1], 10);
+        var dt12 = d1[2].split(" ");
+        var dt22 = d2[2].split(" ");
+        var start_year = parseInt(dt12[0], 10);
+        var end_year = parseInt(dt12[0], 10);
+        var check = false;
+        if (start_month < end_month && start_year <= end_year) {
+            check = true;
+        }
+        if (start_month > end_month && start_year <= end_year) {
+            check = true;
+        }
+
+
+        if ($('#add_title').val().length != 0 && $('#add_start').val().length != 0 && $('#add_end').val().length != 0 && ($('#add_start').val() < $('#add_end').val() || check)) {
+            var newEvent = {
+
+                title: $('#add_title').val(),
+                start: moment($('#add_start').val(), 'DD-MM-YYYY HH:mm').format("LLLL"),
+                end: moment($('#add_end').val(), 'DD-MM-YYYY HH:mm').format("LLLL"),
+                location: $('#add_location').val(),
+                description: $('#add_description').val(),
+                allDay: false,
+                calendarId: calendarId,
+                isPrivate: $('#add_is_private').prop('checked'),
+                isInvite: false,
+                isInviteProcessed: false,
+                invites: []
+            };
+
+            calendar.fullCalendar('renderEvent', newEvent, true);
+            calendar.fullCalendar('unselect');
+
+            $.ajax({
+                type: 'POST',
+                url: '/API/CalendarEvent/CreateNewEvent',
+                data: newEvent
+            });
+
+            $('.add_close').click();
+
+            return false;
+        }
+
+        else {
+
+            if ($('#add_start').val() > $('#add_end').val() && check == false) {
+
+                alert("Der Zeitraum für das Ereignis wurde nicht korrekt eingegeben! Versuchen Sie die Eingabe noch mal.");
+            }
+            if ($('#add_start').val() == $('#add_end').val()) {
+                alert("Sie haben identische Werte für Start und Ende eigegeben. Versuchen Sie die Eingabe noch mal.");
+            }
+            if ($('#add_start').val().length == 0) {
+                alert("Sie haben nichts für Start eingegeben");
+            }
+            if ($('#add_end').val().length == 0) {
+                alert("Sie haben nichts für Ende eingegeben");
+            }
+            if ($('#add_title').val().length == 0) {
+                alert("Sie haben nichts für Title eingegeben");
+            }
+
+            return true;
+
+
         }
     });
 });
